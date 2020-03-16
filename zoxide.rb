47fe0f1f9d18446c58d32aaa04398bda7c48704e
Ajeet D'Souza <98ajeet@gmail.com>
2020-03-16 08:25:20 +0530

    Update zoxide to v0.2.1

--- zoxide.rb
+++ zoxide.rb
@@ -1,8 +1,8 @@
 class Zoxide < Formula
   desc "A fast cd command that learns your habits"
   homepage "https://github.com/ajeetdsouza/zoxide"
-  url "https://github.com/ajeetdsouza/zoxide/archive/v0.2.0.tar.gz"
-  sha256 "628c394d7b5031cf986c5a8b24083dc5941a21d7491ae916684c3322ac748318"
+  url "https://github.com/ajeetdsouza/zoxide/archive/v0.2.1.tar.gz"
+  sha256 "0026622781ae04323512a70eb25939c3134f3e07a1f7a0a5d84d5cbb2d4e5136"
 
   depends_on "rust" => :build
 
